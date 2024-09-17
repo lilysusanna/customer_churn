c28172b951d4ada12ac97c7301495b27efb3b76d
lilysusanna <103573572+lilysusanna@users.noreply.github.com>
2024-09-18 02:51:37 +0530

    Update ML_IIT.py

--- ML_IIT.py
+++ ML_IIT.py
@@ -5,6 +5,8 @@ Created on Mon Nov 14 20:05:13 2022
 @author: Lily, Ajay, Baseet, Adithya, Sudhanshu
 """
 pip install --upgrade
+print(f"Python version: {sys.version}")
+print(f"Version info: {sys.version_info}")
 import streamlit as st
 import pandas as pd
 import numpy as np
